240e347b18ad24e96ea44a66ce8cf6f129ef722a
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-02-15 11:47:57 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const navToggle = documnet.querySelector('.nav-toggle');
+const navLinks = documnet.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
